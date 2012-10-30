e01b26ed59ff795d28aa7ccc6069281f1437e8ed
vitaliy <vitaliy.yarovuy@qa-power.com>
2012-10-31 00:01:54 +0200

    stage-3

--- content/js/togodocs.js
+++ content/js/togodocs.js
@@ -5,18 +5,43 @@
 		this.init();
 	}
 	ToGoDocsControl.prototype.init = function(){
-		var btn, el;
-		btn = utils.createElement("button",{"className":cssPrefix+"-select-button", "innerHTML":"select content"});
-		el = utils.createElement("div",{"className": cssPrefix+"-wrap"},[btn]);
+		var btn, tagGroup, el;
+		el = utils.createElement("div",{"id": cssPrefix},[
+			utils.createElement("div",{"className": "navbar navbar-fixed-top"},[
+				utils.createElement("div",{ className: "navbar-inner"},[
+					utils.createElement("div",{ className: "container"},[
+						utils.createElement("div",{ className: "row"},[
+							utils.createElement("div",{className: "span2"},[
+								btn = utils.createElement("div",{className: "btn", "data-toggle":"button", innerHTML:"select content"})
+							]),
+							utils.createElement("div",{className: "span8"},[
+								utils.createElement("h6",{ className:"pull-left", innerHTML: "TAGS:&nbsp;"}),
+								tagGroup = utils.createElement("div",{className: "btn-group white-space-normal ", "data-toggle":"buttons-radio"},[
+									utils.createElement("div",{className: "btn", "data-toggle":"button", innerHTML:"body"}),
+									utils.createElement("div",{className: "btn", "data-toggle":"button", innerHTML:"div"}),
+									utils.createElement("div",{className: "btn", "data-toggle":"button", innerHTML:"p"}),
+									utils.createElement("div",{className: "btn", "data-toggle":"button", innerHTML:"div"})
+								])
+							]),
+							utils.createElement("div",{className: "span2"},[
+								utils.createElement("div",{className: "btn btn-primary", "data-loading-text":"Saving...", innerHTML:"save"})
+							])
+						])
+					])
+				])
+			])
+		]);
+
 		btn.addEventListener("click",this.onBtnSelectPress.bind(this));
 		document.body.appendChild(el);
 		this.el = el;
 	};
 	ToGoDocsControl.prototype.toggle = function(){
-		this.el.classList.toggle(cssPrefix+"-active")
+		this.el.classList.toggle("active");
 	};
 	ToGoDocsControl.prototype.onBtnSelectPress = function(){
-		alert("button press on page: \n" + location.href );
+
+		alert("button press on page: \n" + location.href + this.classList.contains("active"));
 	};
 
 	window.ToGoDocsControl = ToGoDocsControl;
